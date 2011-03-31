c802cc48d0ad32b340d3c67ed720cc6ff8041e3b
Zach Smith <x.coder.zach@gmail.com>
2011-03-31 12:06:05 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SurveyRails::Application.config.secret_token = 'b55599ca955a15e0e73f328e5df77c36a3ca32577f0dc4764f15b85e01e00f2514a52cc7af64c1d504f4c55928d0846505b8c63bbef0123762e64b7985e761c8'
