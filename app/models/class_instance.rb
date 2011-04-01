fbff00d15e758754318afd53c1e499768f368e62
Zach Smith <x.coder.zach@gmail.com>
2011-04-01 00:34:31 -0500

    finishing class instances

--- app/models/class_instance.rb
+++ app/models/class_instance.rb
@@ -1,5 +1,5 @@
 class ClassInstance < ActiveRecord::Base
   has_many   :groups
-  has_many   :students, :through => "groups", :conditions => {:type => "student"}
   belongs_to :instructor, :class_name => "User", :conditions => {:type => "instructor"}
+  has_many   :students, :through => "groups", :conditions => {:type => "student"}
 end
