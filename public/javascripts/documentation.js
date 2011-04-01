40e690721e07dcbf99b06b3c3dc8c615000e473e
Zach Smith <x.coder.zach@gmail.com>
2011-04-01 01:10:14 -0500

    split the documentation up into more than one page

---
+++ public/javascripts/documentation.js
@@ -0,0 +1,32 @@
+$(function() {
+
+$(".documentation").each(function(index, element) {
+  var method = $(".method", element).text()
+    , link = $(".url", element)
+    , url = link.attr("href")
+    , response = $(".response", element)
+    , data = $(".data .datum", element)
+    , postData = {}
+
+  data.each(function(index, datum) {
+    var key = $(".key", datum).text()
+      , val = $(".value", datum).text()
+    postData[key] = val
+  })
+
+
+  link.click(function(event) {
+    var options = {type: method, dataType: "text"}
+    if(data.length !== 0) {
+      options.data = postData
+    }
+    event.preventDefault()
+    $.ajax(url, options) 
+      .success(function(res) {
+        console.log(res)
+        response.text(res)
+      })
+  })
+}) 
+
+})
