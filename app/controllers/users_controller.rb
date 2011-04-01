88995acc08851c421b9b156913aee50ee2333c45
Zach Smith <x.coder.zach@gmail.com>
2011-03-31 22:26:30 -0500

    all the crud for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,21 @@ class UsersController < ApplicationController
     respond_with @users = User.all
   end
 
-  def create
-    respond_with @user = User.create(params[:user])
+  def show
+    respond_with @user = User.where(:id => params[:id])
   end
 
+  def update
+    respond_with @user = User.update(params[:id], params[:user])
+  end
+
+  def create
+    respond_with @user = User.create(params[:user]), :action => :show
+  end
+
+  def show
+    @user = User.where(:id => params[:id])
+    respond_with User.destroy(@user)
+  end
+ 
 end
