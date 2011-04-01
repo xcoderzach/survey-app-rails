1ddcf6ff408544084dca2683851c81cd004ff6db
Zach Smith <x.coder.zach@gmail.com>
2011-04-01 01:37:39 -0500

    better docs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActionController::RoutingError, :with => :route_not_found
 
+  private
+    
+  def route_not_found 
+    render :xml => {:surveyapp => {:error => "not_found"}}, :status => 404
+  end
 end
